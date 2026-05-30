4aee64eb2652060a599b7d1f2de8a1fae9afe4fa
AJII GODSWILL <godswillajii2025@gmail.com>
2026-05-30 14:41:59 +0100

    case sensitive fix

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -26,64 +26,64 @@ router.delete(
 router.get(
   "/logs",
   protect,
-  rolecheck(["admin"], ["general_admin"]),
+  roleCheck(["admin"], ["general_admin"]),
   checkLogs,
 );
 
 router.get(
   "/all",
   protect,
-  rolecheck(["admin"], ["general_admin"]),
+  roleCheck(["admin"], ["general_admin"]),
   getAllAdmins,
 );
 
 router.post(
   "/:facultyId/departments",
   protect,
-  rolecheck(["admin"], ["registry_admin"]),
+  roleCheck(["admin"], ["registry_admin"]),
   createDepartment,
 );
 router.get(
   "/:facultyId/departments",
   protect,
-  rolecheck(["admin"], ["registry_admin"]),
+  roleCheck(["admin"], ["registry_admin"]),
   getAllDepartments,
 ); // includes stats
 router.patch(
   "/:facultyId/departments/:departmentId",
   protect,
-  rolecheck(["admin"], ["registry_admin"]),
+  roleCheck(["admin"], ["registry_admin"]),
   updateDepartment,
 );
 router.delete(
   "/:facultyId/departments/:departmentId",
   protect,
-  rolecheck(["admin"], ["registry_admin"]),
+  roleCheck(["admin"], ["registry_admin"]),
   deleteDepartment,
 );
 
 router.post(
   "/faculty",
   protect,
-  rolecheck(["admin"], ["registry_admin"]),
+  roleCheck(["admin"], ["registry_admin"]),
   createFacultyEntry,
 );
 router.get(
   "/faculty",
   protect,
-  rolecheck(["admin"], ["registry_admin"]),
+  roleCheck(["admin"], ["registry_admin"]),
   getAllFaculty,
 );
 router.patch(
   "/faculty/:id",
   protect,
-  rolecheck(["admin"], ["registry_admin"]),
+  roleCheck(["admin"], ["registry_admin"]),
   updateFacultyEntry,
 );
 router.delete(
   "/faculty/:id",
   protect,
-  rolecheck(["admin"], ["registry_admin"]),
+  roleCheck(["admin"], ["registry_admin"]),
   deleteFacultyEntry,
 );
 module.exports = router;
