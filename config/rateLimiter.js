5b9b98f64931c9ff35fd0ce6e9e019137bea6772
Yekeen nasir <yekeennasir312@gmail.com>
2026-06-02 05:52:28 +0100

    limit and updating
    limit and updating

--- config/rateLimiter.js
+++ config/rateLimiter.js
@@ -3,7 +3,7 @@ const rateLimit = require("express-rate-limit");
 // General limiter → for all routes
 const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // max 50 requests per window
+  max: 200, // max 50 requests per window
   message: {
     error: "Too many request from this IP, please try again in 15 minutes",
   },
@@ -13,7 +13,7 @@ const generalLimiter = rateLimit({
 
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 3,
+  max: 10,
   message: {
     error: "Too many login attempts, please try again in 15 minutes",
   },
