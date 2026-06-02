ce8793b267f6d8ebdbfe286daa4a676b064f3790
Yekeen nasir <yekeennasir312@gmail.com>
2026-06-02 06:01:51 +0100

    delete hash
    hash.js deleted and included bcrypt

--- hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-const password = "Gadmin2026"; // 👈 change this if needed (e.g. "Gadmin2026")
-
-(async () => {
-  const hash = await bcrypt.hash(password, 10);
-  console.log("HASH:", hash);
-})();
